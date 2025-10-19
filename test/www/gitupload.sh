e37c09add3ed067aa3d0b4bc151db3a0c93d9927
Mauricio Fontana <mfontana@baufest.com>
2025-10-19 16:52:12 -0300

    Removing test and swf dir

--- test/www/gitupload.sh
+++
@@ -1,4 +0,0 @@
-git status
-git add .
-git commit -m "Uploading Whopaid"
-git push origin
--- test/www/js/config.js
+++
@@ -1,7 +0,0 @@
-function Config() {}
-
-Config.C_FOLDER_VIEWS = "views/";
-Config.C_LOG = true;
-Config.C_ANDROID_DEFINED = 0;
-Config.C_DELAY_INSERT_RECORD_MS = 100;   // in miliseconds
-Config.DEBUG = true;
