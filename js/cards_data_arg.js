9e2cfce4deed82d3ede75f96f7c4f4ac8ef87e44
Mauricio Fontana <mfontana@baufest.com>
2025-12-07 22:57:44 -0300

    Moving titles previous to the image

---
+++ js/cards_data_arg.js
@@ -0,0 +1,178 @@
+function populateCardsData_arg(_cards)
+{
+	_cards.push(
+		new CardItem(
+			"id8bitminirally", 
+			"8_bit_minirally",
+			"img/8_bit_minirally_mysite_thumb.png",
+	  	"Porté mi videojuego de QuickBasic 4.5 llamado Minirally a TIC-80, solo por: diversión, aprender Lua y divertirme con la consola TIC-80. NOTA: mi intención no fué hacer algo comercial. NOTA: apretar PLAY para jugar desde itchioo apretar el botón SOURCES.",
+			"https://mfontanadev.itch.io/8-bit-minirally",
+			"https://github.com/mfontanadev/retroProjects/tree/main/8_BIT_MINIRALLY"
+		)
+	)
+
+	_cards.push(
+		new CardItem(
+			"idMemotest", 
+			"Memotest",
+			"img/memotest_mysite_thumb.png",
+			"Este videojuego es una imitación de uno real llamado Memotest. NOTA: la intención no fue hacer algo comercial, está programado usando QuickBasic 4.5. NOTA: para jugarlo se requiere descargarlo (ya que usa un emulador de MSDOS), presionar el botón PLAY para comenzar la descarga desde itchio o presionar el botón SOURCES.",
+			"https://mfontanadev.itch.io/memotest",
+			"https://github.com/mfontanadev/retroProjects/tree/main/MEMOTEST"
+		)
+	)
+
+	_cards.push(
+		new CardItem(
+			"idMinirally", 
+			"Minirally",
+			"img/minirally_mysite_thumb.png",
+			"Este videojuego es una imitación de uno real llamado Mini Rally, está programado usando QuickBasic 4.5 resolucion monocromática (en esos tiempos, 1995). NOTA: para jugarlo se requiere descargarlo (ya que usa un emulador de MSDOS), presionar el botón PLAY para comenzar la descarga desde itchio o presionar el botón SOURCES.",
+			"https://mfontanadev.itch.io/minirally",
+			"https://github.com/mfontanadev/retroProjects/tree/main/MINIRALLY"
+		)
+	)
+
+	_cards.push(
+		new CardItem(
+			"idTetris", 
+			"Tetris",
+			"img/tetris_mysite_thumb.png",
+			"Este videojuego es una imitación del famoso Tetris, está programado usando QuickBasic 4.5 resolucion monocromática (en esos tiempos, 1995). NOTA: para jugarlo se requiere descargarlo (ya que usa un emulador de MSDOS), presionar el botón PLAY para comenzar la descarga desde itchio o presionar el botón SOURCES.",
+			"https://mfontanadev.itch.io/tetris",
+			"https://github.com/mfontanadev/retroProjects/tree/main/TETRIS"
+		)
+	)
+
+	_cards.push(
+		new CardItem(
+			"idTelefono", 
+			"Telefono (Telephone)",
+			"img/telefono_mysite_thumb.png",
+			"Este videojuego es una imitación de uno real llamado Simon pero usando la forma de un teléfono, está programado usando QuickBasic 4.5 resolucion monocromática (en esos tiempos, 1995). NOTA: para jugarlo se requiere descargarlo (ya que usa un emulador de MSDOS), presionar el botón PLAY para comenzar la descarga desde itchio o presionar el botón SOURCES.",
+			"https://mfontanadev.itch.io/telefono",
+			"https://github.com/mfontanadev/retroProjects/tree/main/TELEFONO"
+		)
+	)
+
+	_cards.push(
+		new CardItem(
+			"idLittleConstructorThreeJSEx", 
+			"Little constructor ThreeJSEx",
+			"img/little-constructor-threejs-ex.png",
+			"Es lo mismo que 'Little constructor ThreeJS' con una nueva UI, ahora es más simple de usar. Juega y construye tu propio modelo de casa. Motivado por el tutorial de @javidx9 hice mi propio motor 3D (javascript). Traté de imitar este maravilloso juguete desde que era un niño (ver <a href='http://www.instagram.com/casitas_frm/'>link</a>).",
+			"https://mfontanadev.github.io/games/deployLittleConstructorThreeJSEx/index.html",
+			"https://github.com/mfontanadev/appLittleConstructor/tree/ThreeJSEx"
+		)
+	)
+
+	_cards.push(
+		new CardItem(
+			"idMicroSpy", 
+			"MicroSpy" ,
+			"img/microspy-thumb.jpg",
+			"Videojuego desarrollado para la Jam2020 con @javidx9 como anfitrión. Tu misión es escapar de, -The Great or The Big Machine? o ambas",
+			"https://mfontanadev.itch.io/micro-spy",
+			"https://github.com/mfontanadev/POCs/tree/master/MicroSpy"
+		)
+	)
+	
+	_cards.push(
+		new CardItem(
+			"idRiseOfLadybug_part3", 
+			"Rise of Ladybug Part #3" ,
+			"img/rise-of-ladybug-thumb.jpg",
+			"Parte #3 alias chapter3_gamePage, parte final. Minijuego que sirvió para empujarme a seguir un cronograma con escaso tiempo (máximo 2 horas por día). Ver detaller en Git.",
+			"https://mfontanadev.github.io/games/deployRiseOfLadybug/chapter3_gamePage/index.html",
+			"https://github.com/mfontanadev/POCs/tree/master/RiseOfLadybug/chapter3_gamePage/RiseOfLadybug"
+		)
+	)
+
+	_cards.push(
+		new CardItem(
+			"idRiseOfLadybug_part2", 
+			"Rise of Ladybug Part #2" ,
+			"img/rise-of-ladybug-thumb.jpg",
+			"Parte #2 alias chapter2_interMissionPage. CUIDADO: JUEGO EN CONTRUCCIÓN. Minijuego que sirvió para empujarme a seguir un cronograma con escaso tiempo (máximo 2 horas por día). Ver detaller en Git.",
+			"https://mfontanadev.github.io/games/deployRiseOfLadybug/chapter2_interMissionPage/index.html",
+			"https://github.com/mfontanadev/POCs/tree/master/RiseOfLadybug/chapter2_interMissionPage/RiseOfLadybug"
+		)
+	)
+
+ 	_cards.push(
+		new CardItem(
+			"idRiseOfLadybug_part1", 
+			"Rise of Ladybug Part #1" ,
+			"img/rise-of-ladybug-thumb.jpg",
+			"Parte #1 alias chapter1_menupage. CUIDADO: JUEGO EN CONTRUCCIÓN. Minijuego que sirvió para empujarme a seguir un cronograma con escaso tiempo (máximo 2 horas por día). Ver detaller en Git.",
+			"https://mfontanadev.github.io/games/deployRiseOfLadybug/chapter1_menupage/index.html",
+			"https://github.com/mfontanadev/POCs/tree/master/RiseOfLadybug/chapter1_menupage/RiseOfLadybug"
+		)
+	)
+
+	_cards.push(
+		new CardItem(
+			"idThreeJSSenoid", 
+			"ThreeJS senoid" ,
+			"img/threejs-senoid.png",
+			"Aplicación que muestra algunas funciones z, en este caso senoide, gota de lluevia, Perlin noise, visualmente en forma de 3D, pero realmente es una excusa para jugar y aprender Three.js.",
+			"https://mfontanadev.github.io/games/deployThreeJSSenoid/POC_senoid_threejs.html",
+			"https://github.com/mfontanadev/POCs/tree/master/ThreeJS_senoid"
+		)
+	)
+
+	_cards.push(
+		new CardItem(
+			"idEngineJSSenoid", 
+			"EngineJS senoid" ,
+			"img/engineJS-senoid.jpg",
+			"Aplicación que muestra la fucnion senoide en forma de 3D, aunque realmente es una excusa para jugar con mi motor 3D para javascript basado en los tutoriales de @javidx9 sobre 3D y Perlin noise.",
+			"https://mfontanadev.github.io/games/deployJSEngineSenoid/JSEngine_senoid_v1.0.html",
+			"https://github.com/mfontanadev/POCs/tree/master/JSEngine_senoid"
+		)
+	)
+
+	_cards.push(
+		new CardItem(
+			"idLittleConstructorThreeJS", 
+			"Little constructor ThreeJS",
+			"img/little-constructor-threejs.jpg",
+			"Same as 'Little constructor' with ThreeJS render engine (it looks solid). Play and build your own house model. Following @javidx9 tutorial I made my own 3D Engine (javascript). I tried to modelate this amazing toy from my childhood (see <a href='http://www.instagram.com/casitas_frm/'>link</a>).",
+			"https://mfontanadev.github.io/games/deployLittleConstructorThreeJS/index.html",
+			"https://github.com/mfontanadev/appLittleConstructor/tree/ThreeJS"
+		)
+	)
+
+	_cards.push(
+		new CardItem(
+			"idLittleConstructor", 
+			"Little constructor" ,
+			"img/little-constructor.jpg",
+			"Play and build your own house model. Following @javidx9 tutorial I made my own 3D Engine (javascript). I tried to modelate this amazing toy from my childhood (see <a href='http://www.instagram.com/casitas_frm/'>link</a>).",
+			"http://mfontanadev.github.io/games/deployLittleConstructor/index.html",
+			"https://github.com/mfontanadev/appLittleConstructor"
+		)
+	)
+
+	_cards.push(
+		new CardItem(
+			"idWishFlower", 
+			"Wishflower" ,
+			"img/wishflower.jpg",
+			"Artgame where you can make a wish. Using fractal L-Systems and inspired in 'Plum Park in Kameido'.",
+			"http://wishflower.herokuapp.com",
+			"https://github.com/mfontanadev/wishflower"
+		)
+	)
+
+	_cards.push(
+		new CardItem(
+			"idPoliticFight", 
+			"Politic Fight" ,
+			"img/politic-fight.jpg",
+			"An excuse to make my own platform game engine (javascript). Sprites were supposed to be politician but I'm not good at drawing.",
+			"http://mfontanadev.github.io/games/deployPoliticFight/index.html",
+			"https://github.com/mfontanadev/appPoliticFight"
+		)
+	)
+}
--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ function init(_htmlControlNavBar, _refreshPage)
 		applyLocalization();
 
 		setTimeout(function() {
-			populateCardsData_english(Cards);
+			populateCardsDataWithLocalization(Cards);
 			setCardsHtmlElements(Cards);
 		}, 100);
 	}, 100);
@@ -37,191 +37,22 @@ function CardItem(_id, _title, _image, _description, _appURL, _sourcesURL)
 	this.sourcesURL = _sourcesURL;	
 }
 
-function populateCardsData_english(_cards)
-{
-	_cards.push(
-		new CardItem(
-			"id8bitminirally", 
-			"8_bit_minirally",
-			"img/8_bit_minirally_mysite_thumb.png",
-			"I ported my QuickBasic 4.5 game called Minirally to TIC-80, just for: fun, learn lua and enjoy TIC-80 console. NOTE: it is not intended to be a commercial stuff. NOTE: press PLAY to download from itchio or press SOURCES button.",
-			"https://mfontanadev.itch.io/8-bit-minirally",
-			"https://github.com/mfontanadev/retroProjects/tree/main/8_BIT_MINIRALLY"
-		)
-	)
-
-	_cards.push(
-		new CardItem(
-			"idMemotest", 
-			"Memotest",
-			"img/memotest_mysite_thumb.png",
-			"It is an imitation of the a real game called Memotest. NOTE: it is not intended to be a commercial stuff., programed with QuickBasic 4.5. NOTE: download required, press PLAY to download from itchio or press SOURCES button.",
-			"https://mfontanadev.itch.io/memotest",
-			"https://github.com/mfontanadev/retroProjects/tree/main/MEMOTEST"
-		)
-	)
-
-	_cards.push(
-		new CardItem(
-			"idMinirally", 
-			"Minirally",
-			"img/minirally_mysite_thumb.png",
-			"It is an imitation of the popular real game Mini Rally, programed with QuickBasic 4.5, monochromatic displays (those times). NOTE: download required, press PLAY to download from itchio or press SOURCES button.",
-			"https://mfontanadev.itch.io/minirally",
-			"https://github.com/mfontanadev/retroProjects/tree/main/MINIRALLY"
-		)
-	)
-
-	_cards.push(
-		new CardItem(
-			"idTetris", 
-			"Tetris",
-			"img/tetris_mysite_thumb.png",
-			"It is an imitation of the popular game Tetris, programed with QuickBasic 4.5, monochromatic displays (those times) near 1995. NOTE: download required, press PLAY to download from itchio or press SOURCES button.",
-			"https://mfontanadev.itch.io/tetris",
-			"https://github.com/mfontanadev/retroProjects/tree/main/TETRIS"
-		)
-	)
-
-	_cards.push(
-		new CardItem(
-			"idTelefono", 
-			"Telefono (Telephone)",
-			"img/telefono_mysite_thumb.png",
-			"It is an imitation of the popular game called Simon but using telephone numbers instead. Made with QuickBasic 4.5 in 1995. NOTE: download required, press PLAY to download from itchio or press SOURCES button.",
-			"https://mfontanadev.itch.io/telefono",
-			"https://github.com/mfontanadev/retroProjects/tree/main/TELEFONO"
-		)
-	)
-
-	_cards.push(
-		new CardItem(
-			"idLittleConstructorThreeJSEx", 
-			"Little constructor ThreeJSEx",
-			"img/little-constructor-threejs-ex.png",
-			"Same as 'Little constructor ThreeJS' with new UX, now it is user friendly. Play and build your own house model. Following @javidx9 tutorial I made my own 3D Engine (javascript). I tried to modelate this amazing toy from my childhood (see <a href='http://www.instagram.com/casitas_frm/'>link</a>).",
-			"https://mfontanadev.github.io/games/deployLittleConstructorThreeJSEx/index.html",
-			"https://github.com/mfontanadev/appLittleConstructor/tree/ThreeJSEx"
-		)
-	)
-
-	_cards.push(
-		new CardItem(
-			"idMicroSpy", 
-			"MicroSpy" ,
-			"img/microspy-thumb.jpg",
-			"Game developed for Jam2020 hosted by @javidx9. Your mission is scape from, -The Great or The Big Machine? it is both",
-			"https://mfontanadev.itch.io/micro-spy",
-			"https://github.com/mfontanadev/POCs/tree/master/MicroSpy"
-		)
-	)
-	
-	_cards.push(
-		new CardItem(
-			"idRiseOfLadybug_part3", 
-			"Rise of Ladybug Part #3" ,
-			"img/rise-of-ladybug-thumb.jpg",
-			"Part #3 alias chapter3_gamePage, final part. GAME PLAYABLE, ENJOY. Minigame to push myself to follow a no time schedule (max 2 hours per day) and much more. See datails on Git.",
-			"https://mfontanadev.github.io/games/deployRiseOfLadybug/chapter3_gamePage/index.html",
-			"https://github.com/mfontanadev/POCs/tree/master/RiseOfLadybug/chapter3_gamePage/RiseOfLadybug"
-		)
-	)
-
-	_cards.push(
-		new CardItem(
-			"idRiseOfLadybug_part2", 
-			"Rise of Ladybug Part #2" ,
-			"img/rise-of-ladybug-thumb.jpg",
-			"Part #2 alias chapter2_interMissionPage. WARINIG: GAME UNDER CONSTRUCTION. Minigame to push myself to follow a no time schedule (max 2 hours per day) and much more. See datails on Git.",
-			"https://mfontanadev.github.io/games/deployRiseOfLadybug/chapter2_interMissionPage/index.html",
-			"https://github.com/mfontanadev/POCs/tree/master/RiseOfLadybug/chapter2_interMissionPage/RiseOfLadybug"
-		)
-	)
-
- 	_cards.push(
-		new CardItem(
-			"idRiseOfLadybug_part1", 
-			"Rise of Ladybug Part #1" ,
-			"img/rise-of-ladybug-thumb.jpg",
-			"Part #1 alias chapter1_menupage. WARINIG: GAME UNDER CONSTRUCTION. Minigame to push myself to follow a no time schedule (max 2 hours per day) and much more. See datails on Git.",
-			"https://mfontanadev.github.io/games/deployRiseOfLadybug/chapter1_menupage/index.html",
-			"https://github.com/mfontanadev/POCs/tree/master/RiseOfLadybug/chapter1_menupage/RiseOfLadybug"
-		)
-	)
-
-	_cards.push(
-		new CardItem(
-			"idThreeJSSenoid", 
-			"ThreeJS senoid" ,
-			"img/threejs-senoid.png",
-			"App that shows you some zFunctions like senoid, raindrop, Perlin noise, in a 3D way but, really this is another excuse to play with Three.js.",
-			"https://mfontanadev.github.io/games/deployThreeJSSenoid/POC_senoid_threejs.html",
-			"https://github.com/mfontanadev/POCs/tree/master/ThreeJS_senoid"
-		)
-	)
-
-	_cards.push(
-		new CardItem(
-			"idEngineJSSenoid", 
-			"EngineJS senoid" ,
-			"img/engineJS-senoid.jpg",
-			"App that shows you senoid function in a 3D way but, really this another excuse to play with hyper minimal 3dEngineJS, based on @javidx9 tutorial about 3D and Perlin noise.",
-			"https://mfontanadev.github.io/games/deployJSEngineSenoid/JSEngine_senoid_v1.0.html",
-			"https://github.com/mfontanadev/POCs/tree/master/JSEngine_senoid"
-		)
-	)
-
-	_cards.push(
-		new CardItem(
-			"idLittleConstructorThreeJS", 
-			"Little constructor ThreeJS",
-			"img/little-constructor-threejs.jpg",
-			"Same as 'Little constructor' with ThreeJS render engine (it looks solid). Play and build your own house model. Following @javidx9 tutorial I made my own 3D Engine (javascript). I tried to modelate this amazing toy from my childhood (see <a href='http://www.instagram.com/casitas_frm/'>link</a>).",
-			"https://mfontanadev.github.io/games/deployLittleConstructorThreeJS/index.html",
-			"https://github.com/mfontanadev/appLittleConstructor/tree/ThreeJS"
-		)
-	)
-
-	_cards.push(
-		new CardItem(
-			"idLittleConstructor", 
-			"Little constructor" ,
-			"img/little-constructor.jpg",
-			"Play and build your own house model. Following @javidx9 tutorial I made my own 3D Engine (javascript). I tried to modelate this amazing toy from my childhood (see <a href='http://www.instagram.com/casitas_frm/'>link</a>).",
-			"http://mfontanadev.github.io/games/deployLittleConstructor/index.html",
-			"https://github.com/mfontanadev/appLittleConstructor"
-		)
-	)
-
-	_cards.push(
-		new CardItem(
-			"idWishFlower", 
-			"Wishflower" ,
-			"img/wishflower.jpg",
-			"Artgame where you can make a wish. Using fractal L-Systems and inspired in 'Plum Park in Kameido'.",
-			"http://wishflower.herokuapp.com",
-			"https://github.com/mfontanadev/wishflower"
-		)
-	)
-
-	_cards.push(
-		new CardItem(
-			"idPoliticFight", 
-			"Politic Fight" ,
-			"img/politic-fight.jpg",
-			"An excuse to make my own platform game engine (javascript). Sprites were supposed to be politician but I'm not good at drawing.",
-			"http://mfontanadev.github.io/games/deployPoliticFight/index.html",
-			"https://github.com/mfontanadev/appPoliticFight"
-		)
-	)
-}
-
 function setCardsHtmlElements(_cards)
 {
 	_cards.forEach(cardItem => {
 		injectionCardsText(cardItem);
 	});
 }
+
+function populateCardsDataWithLocalization(_cards) {
+	if (getLanguageCode() === LANG_ARG) {
+		populateCardsData_arg(_cards);
+	}
+	else if (getLanguageCode() === LANG_ENG) {
+		populateCardsData_eng(_cards);
+	}
+}
+
 
 function injectionCardsText(_cardItem)
 {
